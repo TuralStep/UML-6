157a598e763d2d388cd6938708cabb396ee2e741
TuralStep <turalozel@gmail.com>
2022-11-27 15:41:22 +0400

    Added CoR

---
+++ CoR/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace CoR;
+
+
+interface ComponentWithContextualHelp
+{
+    void ShowHelp();
+}
+
+
+abstract class Component : ComponentWithContextualHelp
+{
+    private Container _container;
+    public string tooltipText = null;
+
+    public virtual void ShowHelp()
+    {
+        if (tooltipText != null)
+            Console.WriteLine("Showing Component tooltip...");
+        else
+            _container.ShowHelp();
+    }
+
+}
+
+abstract class Container : Component
+{
+
+    private Component _children;
+
+    public void Add(Component child) { }
+
+}
+
+
+class Button : Component
+{
+    public override void ShowHelp()
+    {
+        Console.WriteLine("Showing Button tooltip...");
+    }
+}
+
+
+class Panel : Container
+{
+    private string _modalHelpText;
+
+    public override void ShowHelp()
+    {
+        if (_modalHelpText != null)
+            Console.WriteLine(_modalHelpText);
+        else
+            base.ShowHelp();
+    }
+
+}
+
+class Dialog : Container
+{
+    private string _wikiPageURL;
+
+    public override void ShowHelp()
+    {
+        if (_wikiPageURL != null)
+            Console.WriteLine(_wikiPageURL);
+        else
+            base.ShowHelp();
+    }
+
+}
